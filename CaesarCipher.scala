8c3de18a61509aa4e4c0292c7cd5030c8fd616b7
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2025-02-20 15:29:03 -0500

    finished Scala

--- CaesarCipher.scala
+++ CaesarCipher.scala
@@ -9,8 +9,6 @@ object CaesarCipher {
     var encrypted: String = ""
     var decrypted: String = ""
 
-
-
     // Function with return value
     def encrypt(s: String, shift: Int): String = {
         var result: String = ""
@@ -19,61 +17,51 @@ object CaesarCipher {
         for (i <- 0 until s.length){
             var ch: String = s.substring(i, i + 1)
             ch = ch.toUpperCase
-            val charPos: Int = atoz.indexOf(ch) + 1  // Convert 0-based to 1-based index, ChatGPT translation of line from Pascal
+            val charPos: Int = atoz.indexOf(ch)  
             // indexOf returns -1 if char not found
             if (charPos != -1) {
-                var index: Int = charPos + shift
-                if (index > 26) {
-                    index = index - 26
-                }
+                // Compute shifted index using mod for proper wrap-around
+                var index: Int = (charPos + shift) % 26
                 // extracts char from string
                 result = result + atoz.substring(index, index + 1)
-                else {
-                    result = result + ch
-                }
+            } else {
+                  result = result + ch
             }
+      
         }
         return result
     }
-
-
 
     // Function with return value
     def decrypt(s: String, shift: Int): String = {
         var result: String = ""
         var i: Int = 0
 
-
         for (i <- 0 until s.length){
             var ch: String = s.substring(i, i + 1)
-            val charPos: Int = atoz.indexOf(ch) + 1  // Convert 0-based to 1-based index, ChatGPT translation of line from Pascal
+            // Using 0-based indexing directly 
+            val charPos: Int = atoz.indexOf(ch)
             // indexOf returns -1 if char not found
             if (charPos != -1) {
-                var index: Int = charPos - shift
-                if (index < 1) {
-                    index = index + 26
-                }
+                // Compute shifted index with wrap-around (adding 26 to avoid negative values)
+                var index: Int = (charPos - shift + 26) % 26
                 // extracts char from string
                 result = result + atoz.substring(index, index + 1)
-                else {
+            } else {
                     result = result + ch
-                }
             }
-            return result
         }
+        return result
+    
     }
-
 
     // Function without return value (procedure)
     def solve(s: String, maxLabel: Int): Unit = {
         var L: Int = 0
         for (L <- maxLabel to 0 by -1){ // by -1 suggested by ChatGPT
-            var currentShift = (stringShift + (26 - L)) % 26
-            println("Caesar: " + L + ": " + decrypt(s, currentShift))
+            println("Caesar " + L + ": " + encrypt(s, L))
         }   
     }
-
-
 
     // Main program execution starts here
     def main(args: Array[String]): Unit = {
@@ -82,14 +70,15 @@ object CaesarCipher {
     
         procString = testString.toUpperCase
         procString = procString.trim
-        println("Test String: ", testString)
+        println("Test String: " + testString)
         encrypted = encrypt(procString, stringShift)
-        println("Encrypted String: ", encrypted)
+        println("Encrypted String: " + encrypted)
         decrypted = decrypt(encrypted, stringShift)
-        println("Decrypted String: ", decrypted)
+        println("Decrypted String: " + decrypted)
         println()
         println("Trying all shifts: ")
-        solve(encrypted, maxShift)
+        // Uses procString so that encryption produces the expected output
+        solve(procString, maxShift)
         println()
     }
 }
